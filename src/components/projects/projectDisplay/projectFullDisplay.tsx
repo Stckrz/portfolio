ca7d5410fb391364038ca248fb5043ac025f0986
Stckrz <rin449@gmail.com>
2024-02-13 12:10:08 -0600

    more style changes.. fixed the broken stuff

--- src/components/projects/projectDisplay/projectFullDisplay.tsx
+++ src/components/projects/projectDisplay/projectFullDisplay.tsx
@@ -1,52 +1,24 @@
 import React from 'react';
 import { useState, useContext } from 'react';
-import { handleFadeOut } from 'library/animationfunctions';
 import style from './projectFullDisplay.module.css';
-import animationStyle from 'library/animationstyles.module.css';
 import { ProjectThumbnailDisplay } from 'components/projects/projectDisplay/projectThumbnailDisplay';
 import { projectsObject } from 'library/projectsObject';
 import { ToolsView } from 'components/toolsview/toolsview';
 import { IProjectInterface } from 'library/context';
 import { FiExternalLink } from 'react-icons/fi';
-import { BackButton } from 'components/backButton/backButton';
-import { CurrentPage } from 'pages/home/home';
 
 interface ProjectFullDisplayProps {
 	project: IProjectInterface,
 }
 
 export const ProjectFullDisplay: React.FC<ProjectFullDisplayProps> = ({ project }) => {
-	const { setPage } = useContext<any>(CurrentPage);
 	const [activeIndex, setActiveIndex] = useState(0);
 
-	//state for this components rendering. used for handling the fade-out of the component upon close
-	const [isMounted, setIsMounted] = useState(true);
 
 	return (
 		<>
-			<div className={isMounted ? animationStyle.componentFadeIn : animationStyle.componentFadeOut}>
 			<div className={style.projectFullDisplayWrapper} style={{ backgroundColor: project.color }} >
 				<div className={style.projectMainBox}>
-
-					<div className={style.indexContainer}>
-						<div className={style.backButtonBox} onClick={() => { handleFadeOut(isMounted, setIsMounted, setPage) }}>
-							<BackButton />
-						</div>
-
-						<div className={style.containerContainer}>
-							{projectsObject.map((project:IProjectInterface) => {
-								return (
-									<div className={style.projectDisplayContainerMin}
-										onClick={() => { setActiveIndex(project.index) }}
-									>
-										<ProjectThumbnailDisplay project={project} />
-									</div>
-								)
-							})}
-						</div>
-
-
-					</div>
 
 					<div className={style.projectScreenshot}>
 						{project.overviewURL !== "" &&
@@ -80,7 +52,6 @@ export const ProjectFullDisplay: React.FC<ProjectFullDisplayProps> = ({ project 
 						</a>
 					</div>
 				</div>
-			</div>
 			</div>
 		</>
 	)
--- src/components/projects/projectsContainer/projectsContainer.tsx
+++ src/components/projects/projectsContainer/projectsContainer.tsx
@@ -4,18 +4,45 @@ import { useState, useContext } from 'react';
 import style from './projectsContainer.module.css';
 import { projectsObject } from 'library/projectsObject';
 import { IProjectInterface } from 'library/context';
+import animationStyle from 'library/animationstyles.module.css';
+
+
+import { BackButton } from 'components/backButton/backButton';
+import { handleFadeOut } from 'library/animationfunctions';
+import { CurrentPage } from 'pages/home/home';
 
 //component imports
 import { ProjectThumbnailDisplay } from 'components/projects/projectDisplay/projectThumbnailDisplay';
 import { ProjectFullDisplay } from 'components/projects/projectDisplay/projectFullDisplay';
 
 export const Projects: React.FC = () => {
-
+	const { setPage } = useContext<any>(CurrentPage);
 	const [activeIndex, setActiveIndex] = useState(0);
+	
+
+	//state for this components rendering. used for handling the fade-out of the component upon close
+	const [isMounted, setIsMounted] = useState(true);
 
 	return (
 		<>
+			<div className={isMounted ? animationStyle.componentFadeIn : animationStyle.componentFadeOut}>
 				<div className={style.projectsWrap}>
+					<div className={style.indexContainer}>
+						<div className={style.backButtonBox} onClick={() => { handleFadeOut(isMounted, setIsMounted, setPage) }}>
+							<BackButton />
+						</div>
+						<div className={style.containerContainer}>
+							{projectsObject.map((project:IProjectInterface) => {
+								return (
+									<div className={style.projectDisplayContainerMin}
+										onClick={() => { setActiveIndex(project.index) }}
+									>
+										<ProjectThumbnailDisplay project={project} />
+									</div>
+								)
+							})}
+						</div>
+						</div>
 					<div className={style.projectsContainer} >
 
 						{projectsObject.map((project:IProjectInterface) => {
@@ -30,18 +57,8 @@ export const Projects: React.FC = () => {
 							)
 						})}
 
-						{/* <div className={style.containerContainer}> */}
-						{/* 	{projectsObject.map((project:IProjectInterface) => { */}
-						{/* 		return ( */}
-						{/* 			<div className={style.projectDisplayContainerMin} */}
-						{/* 				onClick={() => { setActiveIndex(project.index) }} */}
-						{/* 			> */}
-						{/* 				<ProjectThumbnailDisplay project={project} /> */}
-						{/* 			</div> */}
-						{/* 		) */}
-						{/* 	})} */}
-						{/* </div> */}
 					</div>
+				</div>
 				</div>
 		</>
 	)
