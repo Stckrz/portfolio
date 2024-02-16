1cd2fef454e4b1807ea3c5786eaef7f61407fee0
Stckrz <rin449@gmail.com>
2024-02-16 15:02:26 -0600

    some more layout stuff

--- src/components/skillsview/skilldropdown/skilldropdown.tsx
+++ src/components/skillsview/skilldropdown/skilldropdown.tsx
@@ -9,9 +9,15 @@ interface SkillDropdownProps {
 export const SkillDropdown: React.FC<SkillDropdownProps> = ({ icon, title }) => {
 	return (
 		<>
+				<div className={style.skillTitleBox}>
 			<div className={style.iconContainer}>
-				<div className={title === 'React' ? style.reactIcon : style.actualIcon}>{icon}</div>
-				<div>{title}</div>
+					<div className={title === 'React' ? style.reactIcon : style.actualIcon}>{icon}</div>
+					<div>{title}</div>
+				</div>
+				<div className={style.skillDescriptionBox}>
+					<div>this is a description about the stuff..</div>
+				</div>
+				
 			</div>
 		</>
 	)
