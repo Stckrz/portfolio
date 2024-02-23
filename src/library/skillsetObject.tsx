3af7dd38c35d6dfb084b90ea90dc96bfef9f4512
Stckrz <rin449@gmail.com>
2024-02-23 09:04:50 -0600

    added some more stuff to the project, mainly work on the 'skills' section

--- src/library/skillsetObject.tsx
+++ src/library/skillsetObject.tsx
@@ -6,6 +6,7 @@ import { FaPython } from 'react-icons/fa';
 import { FaCss3Alt } from 'react-icons/fa';
 import { FaHtml5 } from "react-icons/fa";
 import { SiTailwindcss } from 'react-icons/si';
+import { FaStackOverflow } from 'react-icons/fa';
 
 import { StackOverflowIcon } from 'components/skillsview/skilldropdown/animations/stackoverflow/stackoverflow';
 const size = "3em"
@@ -14,46 +15,46 @@ export const skillsArray = [
 	{
 		icon: <FaHtml5 size={size} />,
 		title: 'HTML',
-		description: "hello"
+		description: "HTML is something I have continued to learn throughout all of my time learning and developing, and something I imagine I'll continue to learn as time goes on."
 	},
 	{
 		icon: <FaCss3Alt size={size} />,
 		title: 'CSS',
-		description: "hello"
+		description: "CSS is another skill that I continue to learn more and more about as I grow and learn. It's a skill that is naturally improving, because I use it in almost everything I write. Still, its wild how much there is to learn. I like to spend time on codepen sometimes looking at projects, and also sometimes spend time doing CSS battles for fun."
 	},
 	{
 		icon: <IoLogoJavascript size={size} />,
 		title: 'Javascript',
-		description: "hello"
+		description: "Javascript contains the core of my experience with code in general, and it's where I spend the majority of my time. I spent quite a lot of time with javascript, before getting into React or other frameworks."
 	},
 	{
 		icon: <FaReact size={size} />,
 		title: 'React',
-		description: "hello"
+		description: "I have spent more time in the last year in React than anything. I have been pretty much exclusively developing in React for a while now, and absorbing and learning as much as I can. I've done a lot of work with react router, typescript, etc.. I have good experience with function based components, and a good understanding of all of the built-in hooks. I have made a number of web apps in React, some of which are in the projects section of this portfolio."
 	},
 	{
 		icon: <TbBrandTypescript size={size} />,
 		title: 'Typescript',
-		description: "hello"
+		description: "Basically, I always knew I would need typescript from day 1 of my journey with React. I made a number of projects to make sure that I was familiar with React before jumping into typescript, and have made sure to include it in everything I have made sense. They are basically the same thing to me."
 	},
 	{
 		icon: <FaPython size={size} />,
 		title: 'Python',
-		description: "hello"
+		description: "Python was the first language I learned in school, and I spent 2-3 years with it before I left college. Still, I feel that I have a much stronger understanding of Javascript, but my Python skills are servicable. The main thing I use it for now is for creating backends with django/dj-rest-framework."
 	},
 	{
 		icon: <TbBrandDjango size={size} />,
 		title: 'Django',
-		description: "hello"
+		description: "Django was my first experience with any sort of web app framework, and it was introduced as a backend solution. It was confusing at first, but I've spent enough time with it that it's become my go-to solution for making API's and backends in order to test my front-end code."
 	},
 	{
-		icon: <StackOverflowIcon />,
+		icon: <FaStackOverflow size={size} />,
 		title: 'Stack Overflow',
-		description: "hello"
+		description: "This is probably the technology I have the most experience with. Who knows how many countless hours I have spent on this website.."
 	},
 	{
 		icon: <SiTailwindcss size={size} />,
 		title: 'Tailwind',
-		description: "hello"
+		description: "Tailwind is a newer technology to me, but it's very easy to pick up. I've made a handfull of projects with Tailwind, enough to warrant putting in on this list."
 	},
 ]
